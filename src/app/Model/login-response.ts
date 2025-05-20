855cda82db718aca8f3c02c19b0f14b363822e0f
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-20 19:41:31 +0200

    Fix: add missing '.ts' extension to LoginResponse and StandardResponse entities

---
+++ src/app/Model/login-response.ts
@@ -0,0 +1,16 @@
+import { User } from "./User";
+
+export class LoginResponse {
+  user: User;
+  refreshToken: string;
+  accessToken: string;
+
+  constructor(
+    user: User, 
+    refreshToken: string, 
+    accessToken: string) {
+    this.user = user;
+    this.refreshToken = refreshToken;
+    this.accessToken = accessToken;
+  }
+}
