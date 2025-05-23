44b9a7fb48043066b5e367b1efa13308d6caadd1
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-23 11:34:19 +0200

    Fix(User): switch to an object-based constructor and mark idUser and password fields as optional

--- src/app/Model/User.ts
+++ src/app/Model/User.ts
@@ -1,29 +1,27 @@
 import { UserAccountStatus } from "./user-account_status.enum";
-import { UserRole } from "./User-role.enum";
+import { UserRole } from "./user-role.enum";
 
 export class User {
-  idUser: number;
+  idUser?: number;
   username: string;
-  password: string;
+  password?: string;
   fullName: string;
   email: string;
   dateOfBirth: Date;
   roles?: UserRole[];
   accountStatus?: UserAccountStatus;
 
-  constructor(
-    idUser: number,
-    username: string,
-    password: string,
-    fullName: string,
-    email: string,
-    dateOfBirth: Date
-  ) {
-    this.idUser = idUser;
-    this.username = username;
-    this.password = password;
-    this.fullName = fullName;
-    this.email = email;
-    this.dateOfBirth = dateOfBirth;
-  }
+  constructor(params: {
+    username: string;
+    fullName: string;
+    email: string;
+    dateOfBirth: Date;
+    password?: string;
+    idUser?: number;
+    roles?: UserRole[];
+    accountStatus?: UserAccountStatus;
+}) {
+   Object.assign(this, params);
+}
+
 }
