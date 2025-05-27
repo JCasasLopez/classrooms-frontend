58317beb8614f80afb033b75f1d117bce4156499
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-27 19:27:47 +0200

    Refactor(Model): add Role interface to support typed roles in User entity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InitialWindowComponent } from './controller/initial-window/initial-window.component';
+import { LoginComponent } from './controller/login/login.component';
+import { RegisterWindowComponent } from './controller/register-window/register-window.component';
+import { RegisterVerificationComponent } from './controller/register-verification/register-verification.component';
+import { ForgotPasswordComponent } from './controller/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './controller/reset-password/reset-password.component';
+
+// Main app container after login
+import { MenuComponent } from './controller/menu/menu.component';
+
+// Components shown within the "Manage Account" section
+import { DeleteAccountComponent } from './controller/manage-account/delete-account/delete-account.component';
+import { ChangePasswordComponent } from './controller/manage-account/change-password/change-password.component';
+import { UpgradeUserComponent } from './controller/manage-account/upgrade-user/upgrade-user.component';
+import { UpdateStatusComponent } from './controller/manage-account/update-status/update-status.component';
+
+const routes: Routes = [
+  // Public routes
+  { path: '', component: InitialWindowComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterWindowComponent },
+  { path: 'verifyEmail', component: RegisterVerificationComponent },
+  { path: 'forgotPassword', component: ForgotPasswordComponent },
+  { path: 'resetPassword', component: ResetPasswordComponent },
+
+  // Protected area after login
+  {
+    path: 'main',
+    component: MenuComponent,
+    children: [
+      { path: 'manage-account/delete', component: DeleteAccountComponent },
+      { path: 'manage-account/change-password', component: ChangePasswordComponent },
+      { path: 'manage-account/upgrade', component: UpgradeUserComponent },
+      { path: 'manage-account/update-status', component: UpdateStatusComponent },
+      { path: '', redirectTo: 'manage-account/delete', pathMatch: 'full' } // default child route
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { InitialWindowComponent } from './controller/initial-window/initial-window.component';
+import { LoginComponent } from './controller/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { RegisterWindowComponent } from './controller/register-window/register-window.component';
+import { RegisterVerificationComponent } from './controller/register-verification/register-verification.component';
+import { ForgotPasswordComponent } from './controller/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './controller/reset-password/reset-password.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { MenuComponent } from './controller/menu/menu.component';
+import { DeleteAccountComponent } from './controller/manage-account/delete-account/delete-account.component';
+import { ChangePasswordComponent } from './controller/manage-account/change-password/change-password.component';
+import { UpgradeUserComponent } from './controller/manage-account/upgrade-user/upgrade-user.component';
+import { UpdateStatusComponent } from './controller/manage-account/update-status/update-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialWindowComponent,
+    LoginComponent,
+    RegisterWindowComponent,
+    RegisterVerificationComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    MenuComponent,
+    DeleteAccountComponent,
+    ChangePasswordComponent,
+    UpgradeUserComponent,
+    UpdateStatusComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center',
+      timeOut: 5000,
+      closeButton: true,
+      progressBar: true,
+      preventDuplicates: true,
+    }),
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
