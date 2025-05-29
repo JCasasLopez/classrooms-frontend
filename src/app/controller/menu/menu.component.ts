73e0a1b71a90bad0d5ca3af58a4f14f11c4322f2
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-05-29 20:15:19 +0200

    Feat(MenuComponent): add logic to include call to 'delete account' component and correct CSS

--- src/app/controller/menu/menu.component.ts
+++ src/app/controller/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../service/authentication.service';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,16 +12,43 @@ import { AuthenticationService } from '../../service/authentication.service';
 export class MenuComponent {
 
   constructor(
-    private authService: AuthenticationService,
-    private router: Router
+    private authenticationService: AuthenticationService,
+    private toastr: ToastrService,
+    private router: Router,
+    private userService: UserService
   ) {}
 
   onLogout(): void {
-    this.authService.logout();
+    this.authenticationService.logout();
     this.router.navigate(['/login']);
   }
 
   get isAdmin(): boolean {
-    return this.authService.isAdmin();
+    return this.authenticationService.isAdmin();
   }
+
+  confirmDeleteAccount(): void {
+  const confirmDelete = confirm('Are you sure you want to delete your account? This action cannot be undone.');
+  
+  if (confirmDelete) {
+    let accessToken = this.authenticationService.getAccessToken();
+    this.userService.deleteAccount(accessToken)
+      .subscribe({
+        next: (response) => {
+          this.toastr.success(`Message: ${response.message}`, `Status: ${response.status}`);
+          this.authenticationService.logout();
+          setTimeout(() => {
+            this.router.navigate(['/']); 
+          }, 2000);
+        },
+        error: () => {
+        }
+      });
+  }
+}
+
+toggleDropdown() {
+  const dropdown = document.querySelector('.menu-dropdown');
+  dropdown?.classList.toggle('active');
+}
 }
